2e15f3360a7a492824521813e015648f1102c32a
诺离 <85913323@qq.com>
2021-08-15 06:14:13 +0800

    完善功能

--- main.py
+++ main.py
@@ -6,14 +6,27 @@ import string
 import sys
 import requests
 import os
-
+import json
 from fastapi import FastAPI, Query
 import uvicorn as u
 from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
+proxies = {
+    "http": "",
+    "https": "",
+}
 
+token = "9d0c962d47256ad167d3197a4866a071"
+
+
+def readFileStr(filename):
+    str = ""
+    with open(filename,'r') as f:
+        str+=f.read()
+    return str
+    
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -21,24 +34,56 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get('/index')
 async def read_index(request: Request):
+    regJs = readFileStr("regJs.txt")
+
     file = open("email.txt")
     listArr = []
+    
     while True:
         text = file.readline()  # 只读取一行内容
         if not text:
             break
-        print(text)
-        listArr.insert(0,text)
-    file.close()
+        arr = text.split("----")
+        emailUser = {}
+        emailUser['user'] = arr[0]
+        emailUser['pass'] = arr[1]
+        listArr.insert(0,emailUser)
+    file.close() 
+
     
-    return templates.TemplateResponse("index.html", {"request": request, "listArr": listArr})
+    return templates.TemplateResponse("index.html", {"request": request, "listArr": listArr,"regJs":regJs})
+
+@app.get("/sms_getcode")
+async def sms_newmobile(
+    *,
+    mobile:str
+):
+    code = ""
+    try:
+        ret = requests.get("http://api.xiaobai188.com:82/apih5/getUserSmsList?token="+token+"&limit=20&phone=&page=1",proxies=proxies)
+        retJson = json.loads(ret.text)
+        if(retJson['data'][0]['phone'] == mobile):
+            code = retJson['data'][0]['code']
+    except:
+        pass
+
+    return code
+
+
+@app.get("/sms_newmobile")
+async def sms_newmobile():
+    ret = requests.get("http://api.xiaobai188.com:82/apih5/getPhone?token="+token+"&sid=4410&type=0&pr=&ci=&sr=0&haoduan=",proxies=proxies)
+    retJson = json.loads(ret.text)
+    mobile = retJson['data'][0]['phone']
+    return mobile
 
 @app.get('/email')
-async def email(
+async def email( 
     *,
     user: str
 ):
     val = ""
-    with os.popen('python3 imap.py '+user) as p:
+  
+    with os.popen('python imap.py '+user) as p:
         val = p.read()
     return val.replace("\n","")
--- sms.py
+++ sms.py
@@ -7,15 +7,22 @@ proxies = {
     "https": "",
 }
 
-ret = requests.get("http://api.xiaobai188.com:82/apih5/getPhone?token=5eda78b411b4de25041627ea8a9b3253&sid=4410&type=0&pr=&ci=&sr=0&haoduan=",proxies=proxies)
+token = "9d0c962d47256ad167d3197a4866a071"
+
+ret = requests.get("http://api.xiaobai188.com:82/apih5/getPhone?token="+token+"&sid=4410&type=0&pr=&ci=&sr=0&haoduan=",proxies=proxies)
 retJson = json.loads(ret.text)
 mobile = retJson['data'][0]['phone']
 print(mobile)
 while True:
     try:
-        ret = requests.get("http://api.xiaobai188.com:82/apih5/getMsg?token=5eda78b411b4de25041627ea8a9b3253&phone="+ mobile+"&sid=4410",proxies=proxies)
-        #retJson = json.loads(ret.text)
-        print(ret.text)
+        ret = requests.get("http://api.xiaobai188.com:82/apih5/getUserSmsList?token="+token+"&limit=20&phone=&page=1",proxies=proxies)
+        retJson = json.loads(ret.text)
+        if(retJson['data'][0]['phone'] == mobile):
+            print("yes:" + retJson['data'][0]['code'])
+            break
+        else:
+            print("nonono:" + retJson['data'][0]['phone'])
+
     except:
         pass
     time.sleep(4)
